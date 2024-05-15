18bd005fe09b919197bfad90875a10c23bbbfaaa
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-05-15 16:14:03 +0500

    commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let newArray = []
+function sorting() {
+    let userInput = document.getElementById("user").value;
+    let arr = userInput.split(',');
+     newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(i);
+        for (let j = i; j < arr.length; j++) {
+            // console.log(j);
+           let data = 0;
+           if (arr[i] > arr[j]) {
+            // console.log(arr[i]);
+                data = arr[i];
+                arr[i] = arr[j];
+                // console.log(arr[i]);
+                arr[j] = data; 
+                // console.log(arr[j]);
+           }
+        }
+        newArray.push(arr[i]);
+    }
+    document.getElementById("result").value = newArray
+}
+function descending() {
+    let reve = newArray.slice().reverse()
+    document.getElementById("result").value = reve
+}
